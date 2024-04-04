5ff80356bd8231a5337d1e8280b3b320e7f5435e
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-03 22:24:26 -0500

    Create 2803.py

---
+++ 2803/2803.py
@@ -0,0 +1,6 @@
+# bee 2803 - Estados do Norte
+# Alonso Martins
+# 25/03/2024
+norte = ["roraima", "acre", "amapa", "amazonas", "para", "rondonia", "tocantins"]
+estado = input()
+print("Regiao Norte" if estado in norte else "Outra regiao")
