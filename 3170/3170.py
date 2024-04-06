3c7a512210b4630ce1fadc3686a39d3b0e116eaf
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-05 23:38:31 -0500

    Create 3170.py

---
+++ 3170/3170.py
@@ -0,0 +1,6 @@
+# bee 3170 - Bolinhas de Natal
+# Alonso Martins
+# 02/09/2023
+B, G = int(input()), int(input())
+b = G//2-B
+print("Amelia tem todas bolinhas!" if b <= 0 else f"Faltam {b} bolinha(s)")
