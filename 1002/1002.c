3a5e68643e18fb0a0666a6c18df7274805233eb2
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-09 21:21:22 -0500

    Create 1002.c

---
+++ 1002/1002.c
@@ -0,0 +1,13 @@
+// bee 1002 - Area do Circulo
+// Alonso Martins
+// 27/09/2023
+#include <stdio.h>
+ 
+int main() {
+    double pi = 3.14159;
+    double r;
+    scanf("%lf", &r);
+    double A = pi*r*r;
+    printf("A=%.4f\n", A);
+    return 0;
+}
