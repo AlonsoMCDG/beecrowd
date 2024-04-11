d28a83780992256452f1fb384775e5d8fd8c3ad9
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-11 02:14:59 -0500

    Create 1059.c

---
+++ 1059./1059.c
@@ -0,0 +1,10 @@
+// bee 1059 - Numeros Pares
+// Alonso Martins
+// 27/01/2023
+#include <stdio.h>
+
+int main() {
+    for(int i = 2; i <= 100; i += 2)
+        printf("%d\n", i);
+    return 0;
+}
