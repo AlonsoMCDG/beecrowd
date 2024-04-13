21094e3787328d469ee0016b7e3b87174173e836
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-12 23:15:37 -0500

    Create 1000.java

---
+++ 1000/1000.java
@@ -0,0 +1,10 @@
+// bee 1000 - Hello World
+// Alonso Martins
+// 13/04/2024
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
