067e98d8719e8327aff7a59d9b06e336826d914f
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-14 22:42:38 -0500

    Create 1004_1.cpp

---
+++ 1004/1004_1.cpp
@@ -0,0 +1,13 @@
+// bee 1004 - Produto Simples
+// Alonso Martins
+// 03/07/2022
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("PROD = %d\n", (A*B));
+    return 0;
+}
