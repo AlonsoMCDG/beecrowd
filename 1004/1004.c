4922daf9f0cabd36e7c6b50582ff94cc296041c1
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-14 22:41:18 -0500

    Create 1004.c

---
+++ 1004/1004.c
@@ -0,0 +1,15 @@
+// bee 1004 - Produto Simples
+// Alonso Martins
+// 14/04/2024
+#include <stdio.h>
+ 
+int main() 
+{
+    int A, B;
+    
+    scanf("%d %d", &A, &B);
+    
+    printf("PROD = %d\n", (A*B));
+    
+    return 0;
+}
