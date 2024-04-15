a7713814c741ddab7c43c9df721e194f9a43d580
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-14 22:40:00 -0500

    Create 1004.cpp

---
+++ 1004/1004.cpp
@@ -0,0 +1,15 @@
+// bee 1004 - Produto Simples
+// Alonso Martins
+// 14/04/2024
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << "PROD = " << A * B << '\n';
+
+    return 0;
+}
