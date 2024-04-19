f1419b6da2f588c79603344fdaa55fd436069bb2
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-19 01:37:51 -0500

    Create 2619.sql

---
+++ 2619/2619.sql
@@ -0,0 +1,13 @@
+-- bee 2619 - Super Luxo
+-- Alonso Martins
+-- 19/04/2024
+SELECT
+	p.name, pr.name, p.price
+FROM
+	products p
+		INNER JOIN
+	providers pr ON pr.id = p.id_providers
+		INNER JOIN
+	categories c ON c.id = p.id_categories
+WHERE
+	p.price > 1000 AND c.name = 'Super Luxury';
