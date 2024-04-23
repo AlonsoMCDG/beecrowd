9611dd69aac4d783c6898cf3e8014a5335043c9c
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-22 20:11:25 -0500

    Create 1001.cpp

---
+++ 1001/1001.cpp
@@ -0,0 +1,14 @@
+// bee 1001 - Extremamente Basico
+// Alonso Martins
+// 27/09/2023
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("X = %d\n", (A+B));
+ 
+    return 0;
+}
