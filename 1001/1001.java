49b881a9cd9c9282258ad1ae44530775c980aad5
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-22 20:07:32 -0500

    Create 1001.java

---
+++ 1001/1001.java
@@ -0,0 +1,11 @@
+// bee 1001 - Extremamente Basico
+// Alonso Martins
+// 27/09/2023
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt();
+        System.out.println("X = "+(a+b));
+    }
+}
