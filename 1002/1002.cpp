995476646b5f0f27053bae72c67764dd252334e0
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-22 20:13:54 -0500

    Create 1002.cpp

---
+++ 1002/1002.cpp
@@ -0,0 +1,13 @@
+// bee 1002 - Area do Circulo
+// Alonso Martins
+// 03/07/2022
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("SOMA = %d\n", (A+B));
+    return 0;
+}
