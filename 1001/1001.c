59136d157296e8dc43d26c858fba5eacd40a35ac
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-04-22 20:10:46 -0500

    Create 1001.c

---
+++ 1001/1001.c
@@ -0,0 +1,11 @@
+// bee 1001 - Extremamente Basico
+// Alonso Martins
+// 27/09/2023
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("X = %d\n", a + b);
+    return 0;
+}
