57a63b3c19494c1a7a640fd7dc0638de0808edc9
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-05-06 11:34:55 -0500

    Update 2988.sql

--- 2988/2988.sql
+++ 2988/2988.sql
@@ -3,43 +3,43 @@
 -- 19/04/2024
 SELECT 
     t.name,
-    COUNT(DISTINCT m.id) AS matches,
-    SUM(CASE
+	COUNT(DISTINCT m.id) AS matches,
+	SUM(CASE
 			WHEN t.id = m.team_1 AND m.team_1_goals > m.team_2_goals THEN 1
-            WHEN t.id = m.team_2 AND m.team_2_goals > m.team_1_goals THEN 1
+			WHEN t.id = m.team_2 AND m.team_2_goals > m.team_1_goals THEN 1
 			ELSE 0
 		END
 	),
-    SUM(CASE
+	SUM(CASE
 			WHEN t.id = m.team_1 AND m.team_1_goals < m.team_2_goals THEN 1
-            WHEN t.id = m.team_2 AND m.team_2_goals < m.team_1_goals THEN 1
+			WHEN t.id = m.team_2 AND m.team_2_goals < m.team_1_goals THEN 1
 			ELSE 0
 		END
 	),
-    SUM(CASE
+	SUM(CASE
 			WHEN m.team_1_goals = m.team_2_goals THEN 1
 			ELSE 0
 		END
 	),
-    SUM(CASE
-            WHEN t.id = m.team_1 AND m.team_1_goals > m.team_2_goals THEN 3
-            WHEN t.id = m.team_2 AND m.team_2_goals > m.team_1_goals THEN 3
-            WHEN m.team_1_goals = m.team_2_goals THEN 1
-            ELSE 0
-        END
-    )
+	SUM(CASE
+			WHEN t.id = m.team_1 AND m.team_1_goals > m.team_2_goals THEN 3
+			WHEN t.id = m.team_2 AND m.team_2_goals > m.team_1_goals THEN 3
+			WHEN m.team_1_goals = m.team_2_goals THEN 1
+			ELSE 0
+		END
+	)
 FROM
-    teams t
+	teams t
 INNER JOIN
-    matches m ON t.id = m.team_1 OR t.id = m.team_2
+	matches m ON t.id = m.team_1 OR t.id = m.team_2
 GROUP BY 
-    t.id, t.name
+	t.id, t.name
 ORDER BY 
-    SUM(
-        CASE
-            WHEN t.id = m.team_1 AND m.team_1_goals > m.team_2_goals THEN 3
-            WHEN t.id = m.team_2 AND m.team_2_goals > m.team_1_goals THEN 3
-            WHEN m.team_1_goals = m.team_2_goals THEN 1
-            ELSE 0
-        END
-    ) DESC;
+	SUM(
+		CASE
+			WHEN t.id = m.team_1 AND m.team_1_goals > m.team_2_goals THEN 3
+			WHEN t.id = m.team_2 AND m.team_2_goals > m.team_1_goals THEN 3
+			WHEN m.team_1_goals = m.team_2_goals THEN 1
+			ELSE 0
+		END
+	) DESC;
