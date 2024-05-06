76ac2a82ed7c8e1260286eb3112f42267ace4799
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-05-05 20:55:25 -0500

    Update 2997.sql

--- 2997/2997.sql
+++ 2997/2997.sql
@@ -2,25 +2,25 @@
 -- Alonso Martins
 -- 05/05/2024
 SELECT
-	dp.nome AS departamento, 
+    dp.nome AS departamento, 
     e.nome AS empregado,
     salario_empregado.salario_bruto, 
     desconto_empregado.desconto,
     salario_empregado.salario_bruto - desconto_empregado.desconto AS salario_liquido
 FROM
-	empregado e
+    empregado e
     LEFT JOIN (SELECT e.matr, SUM(COALESCE(ds.valor, 0)) AS desconto
-				FROM empregado e
-					LEFT JOIN emp_desc ed ON ed.matr = e.matr
-					LEFT JOIN desconto ds ON ds.cod_desc = ed.cod_desc
-				GROUP BY e.matr
-				) AS desconto_empregado ON desconto_empregado.matr = e.matr
-	LEFT JOIN (SELECT e.matr, SUM(COALESCE(v.valor, 0)) AS salario_bruto
-				FROM empregado e
-					LEFT JOIN emp_venc ev ON ev.matr = e.matr
+                FROM empregado e
+                    LEFT JOIN emp_desc ed ON ed.matr = e.matr
+                    LEFT JOIN desconto ds ON ds.cod_desc = ed.cod_desc
+                GROUP BY e.matr
+                ) AS desconto_empregado ON desconto_empregado.matr = e.matr
+    LEFT JOIN (SELECT e.matr, SUM(COALESCE(v.valor, 0)) AS salario_bruto
+                FROM empregado e
+                    LEFT JOIN emp_venc ev ON ev.matr = e.matr
                     LEFT JOIN vencimento v ON v.cod_venc = ev.cod_venc
-				GROUP BY e.matr
-				) AS salario_empregado ON salario_empregado.matr = e.matr
+                GROUP BY e.matr
+                ) AS salario_empregado ON salario_empregado.matr = e.matr
     LEFT JOIN departamento dp ON dp.cod_dep = e.lotacao
     LEFT JOIN divisao dv ON dv.cod_divisao = e.lotacao_div
     GROUP BY dp.nome, dv.nome, e.nome, salario_empregado.salario_bruto, desconto_empregado.desconto
