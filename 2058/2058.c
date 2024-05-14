8257dc4af385fa00e3caede2ae45fd18776bc446
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-05-14 07:46:17 -0500

    Create 2058.c

---
+++ 2058/2058.c
@@ -0,0 +1,15 @@
+// bee 2058 - Triangulos e Poligonos Regulares
+// Alonso Martins
+// 14/05/2024
+#include <stdio.h>
+
+int main()
+{
+    int N;
+
+    scanf("%d", &N);
+
+    printf("%d\n", N - 2);
+    
+    return 0;
+}
