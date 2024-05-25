428a8d7978c7e6ca16ed89b1301b11049fc2aa67
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-05-25 18:00:47 -0500

    Create main.c

---
+++ Maratona SBC 2024/fase 0/A/main.c
@@ -0,0 +1,16 @@
+// Maratona SBC 2024 - fase 0
+// questão A - Alice e as Canções de Iara
+// Alonso Martins
+// 25/05/2024
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    printf("%d\n", 6 - (A + B + C));
+    
+    return 0;
+}
