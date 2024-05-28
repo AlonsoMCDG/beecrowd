97948e87c18298489ba6626f9b58bfc405124456
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-05-27 19:59:38 -0500

    Create 1715.c

---
+++ 1715/1715.c
@@ -0,0 +1,19 @@
+// bee 1715 - Handebol
+// Alonso Martins
+// 27/05/2024
+#include <stdio.h>
+
+int main()
+{
+    int N, M, i, cont = 0, gols, ans = 0;
+
+    scanf("%d %d", &N, &M);
+
+    while (ans += cont == M, N --)
+        for (cont = i = 0; i < M; i ++)
+            scanf("%d", &gols), cont += gols > 0;
+
+    printf("%d\n", ans);
+
+    return 0;
+}
