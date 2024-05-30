4b369b0bbac66fbddf441f32fb9ef270d553f8b3
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-05-30 18:28:54 -0500

    Create 1795.c

---
+++ 1795/1795.c
@@ -0,0 +1,16 @@
+// bee 1795 - Triangulo Trinomial
+// Alonso Martins
+// 30/05/2024
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int N;
+
+    scanf("%d", &N);
+
+    printf("%lld\n", (long long)pow(3, N));
+
+    return 0;
+}
