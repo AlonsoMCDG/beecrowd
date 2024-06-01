f11cff9fe0507d1702e9dca7b56338ac54e2f7ce
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-06-01 16:22:14 -0500

    Create 2349.c

---
+++ 2349/2349.c
@@ -0,0 +1,21 @@
+// bee 2349 - Farm Robot
+// Alonso Martins
+// 01/06/2024
+#include <stdio.h>
+
+int main()
+{
+    int N, i, C, S, p, r = 0, ans;
+
+    scanf("%d %d %d", &N, &C, &S), S --;
+    
+    for (i = 0, ans = (r == S); i < C && scanf("%d", &p); i ++)
+    {
+        r = (N + r + p) % N;
+        ans += (r == S);
+    }
+    
+    printf("%d\n", ans);
+
+    return 0;
+}
