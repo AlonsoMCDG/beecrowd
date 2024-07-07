cac122f575ee2269027b8d4f8c6b1be890bf289b
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-07-06 22:38:53 -0500

    Create 2372.c

---
+++ 2372/2372.c
@@ -0,0 +1,50 @@
+// bee 2372 - Reuniao
+// Alonso Martins
+// 06/07/2024
+#include <stdio.h>
+#include <math.h>
+
+#define INF 100000
+
+int main()
+{
+    int N, M, i, j, u, v, W, k, ans = INF;
+
+    scanf("%d %d", &N, &M);
+
+    int dist[N][N];
+    int maxdist[N];
+
+    for (i = 0; i < N; i ++)
+    {
+        for (j = 0; j < N; j ++)
+            dist[i][j] = INF * (i != j);
+        maxdist[i] = 0;
+    }
+
+    for (i = 0; i < M; i ++)
+    {
+        scanf("%d %d %d", &u, &v, &W);
+        dist[u][v] = dist[v][u] = W;
+    }
+
+    //// Algoritmo de Floyd-Warshall
+    for (k = 0; k < N; k ++)
+        for (u = 0; u < N; u ++)
+            for (v = 0; v < N; v ++)
+                if (dist[u][v] > dist[u][k] + dist[k][v])
+                    dist[u][v] = dist[u][k] + dist[k][v];
+    ////
+
+    for (i = 0; i < N; i ++)
+    {
+        int d = 0;
+        for (j = 0; j < N; j ++)
+            d = (int)fmax(d, dist[i][j]);
+        ans = (int)fmin(d, ans);
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
+}
