7251174dfce1092e9f14806a3d22719947344d45
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-08-03 13:40:00 -0500

    Update 1708.c

--- 1708/1708.c
+++ 1708/1708.c
@@ -13,21 +13,6 @@ int main()
     printf("%d\n", (int)ceil(1 / (1 - X / Y)));
 
     return 0;
-}// bee 1708 - Volta
-// Alonso Martins
-// 21/07/2024
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    double X, Y;
-
-    scanf("%lf %lf", &X, &Y);
-
-    printf("%d\n", (int)ceil(1 / (1 - X / Y)));
-
-    return 0;
 }
 
 /*
