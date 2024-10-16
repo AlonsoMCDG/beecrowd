c0f6699b92a35c0a8ac9ca19e15e81093fe60a0a
Alonso Martins <110140005+AlonsoMCDG@users.noreply.github.com>
2024-10-15 19:41:45 -0500

    Create 3474.c

---
+++ 3474/3474.c
@@ -0,0 +1,15 @@
+// bee 3474 - Boa divisao
+// Alonso Martins
+// 15/10/2024
+#include <stdio.h>
+
+int main()
+{
+    double V, N;
+
+    scanf("%lf %lf", &V, &N);
+
+    printf("%.2lf\n", V / N);
+
+    return 0;
+}
